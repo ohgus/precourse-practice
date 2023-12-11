dd9d3da8086590f47f916f96f0c26d64887479b1
ohgus <anakin1108@naver.com>
2023-12-11 17:14:25 +0900

    feat: 플래너 시작 메시지 출력 기능 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,7 +1,14 @@
+import { Console } from "@woowacourse/mission-utils";
+import { MESSAGE } from "./constants/messages.js";
+
 export default OutputView = {
-    printMenu() {
-        Console.print("<주문 메뉴>");
-        // ...
-    }
+  printStart() {
+    Console.print(MESSAGE.print.start);
+  },
+
+  printMenu() {
+    Console.print("<주문 메뉴>");
     // ...
-}
+  },
+  // ...
+};
