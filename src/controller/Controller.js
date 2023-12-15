fe73dcc6581500838a3c9b81fce355c598467774
ohgus <anakin1108@naver.com>
2023-12-15 18:49:19 +0900

    feat: 주문 입력 가져오는 기능 추가

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,9 +1,11 @@
 import OutputView from "../view/OutputView.js";
 import InputView from "../view/InputView.js";
 import Callender from "../model/Callender.js";
+import Order from "../model/Order.js";
 
 class Controller {
   #visitDate;
+  #orderInfo;
 
   constructor() {}
 
@@ -16,9 +18,20 @@ class Controller {
     try {
       const date = await InputView.readDate();
       this.#visitDate = new Callender(date).getVisitDate();
+      return await this.#getOrderInput();
     } catch (error) {
       OutputView.printError(error.message);
       return await this.#getDateInput();
+    }
+  }
+
+  async #getOrderInput() {
+    try {
+      const orderInput = await InputView.readOrders();
+      this.#orderInfo = new Order(orderInput);
+    } catch (error) {
+      OutputView.printError(error.message);
+      return await this.#getOrderInput();
     }
   }
 }
